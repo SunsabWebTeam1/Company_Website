34bffd5e30d57a350b9e4645a76e51938815d30c
Jhio0 <sorianojhio218@gmail.com>
2024-05-10 13:14:39 -0600

    remove Faq, about, service in the nav bar and fixing import issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Footer from "./components/Footer";
 import { AnimatePresence } from "framer-motion";
 
 import { Routes, Route, useLocation } from "react-router-dom"
 import Expertise from "./components/Expertise";
 import LandingPage from "./components/LandingPage";
+import Portfolio from "./components/Portfolio";
+import Team from "./components/Team";
+import Contact from "./components/Contact"
+import Footer from "./components/Footer";
 
 import "./App.css"; // Already existing global styles
 import "./styles/style.css"; // Assuming you placed style.css under src/styles/
@@ -27,13 +27,10 @@ function App() {
         <AnimatePresence mode="wait">
         <Routes location={location} key={location.path}>
           <Route path="/landingpage" element={<LandingPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
           <Route path="/expertise" element={<Expertise />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/team" element={<Team />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/faq" element={<FAQ />} />
         </Routes>
         
         </AnimatePresence>

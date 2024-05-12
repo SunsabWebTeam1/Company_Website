0bf11b39b11c35dc9a9532fcc85bc766f8aec730
Austin Huynh <Austin.h54321@gmail.com>
2024-05-11 21:16:25 -0600

    Styling done

---
+++ src/components/MemberCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaLinkedinIn } from "react-icons/fa";
+import "../styles/Membercard.css";
+
+// Defines the member card as a separate component.
+function MemberCard({ name, title, img, linkedin }) {
+  return (
+    <div className="">
+      <div className="card__border">
+        <img
+          className="card__img"
+          src={require(`../images/profilePic/${img}`)}
+          alt={`${name} headshot`}
+        />
+      </div>
+
+      <h3 class="card__name">{name}</h3>
+      <span class="card__profession">{title}</span>
+      <div className="icons">
+        <a href={linkedin} class="card__social-link">
+          <FaLinkedinIn color="#Fff" fontSize="2em" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default MemberCard;
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,44 +1,28 @@
 import React from "react";
 import transition from "../transition";
+import TeamList from "../sunsabMembers";
+import MemberCard from "./MemberCard";
+
 function Team() {
   return (
-    <section className="team" style={{ marginTop: "7rem" }}>
-      <h1 className="heading" style={{ marginTop: "-1.5rem" }} id="team">
-        our team
+    <section className="team" style={{ background: "#fff" }}>
+      <h1 className="heading" style={{ marginTop: "7rem" }} id="team">
+        OUR TEAM
       </h1>
+      <br />
       <div className="row">
-        {/* Team Member Card */}
-        <div className="card">
-          <div className="image">
-            <img
-              src="./images/pexels-andrea-piacquadio-845434.jpg"
-              alt="Team member 1"
-            />
-          </div>
-          <div className="info">
-            <h3>Schott Watkins</h3>
-            <span>Web Developer</span>
-            <div className="icons">
-              <a
-                href="https://www.facebook.com/"
-                className="fab fa-facebook-f"
-              ></a>
-              <a
-                href="https://twitter.com/login"
-                className="fab fa-twitter"
-              ></a>
-              <a
-                href="https://www.instagram.com/"
-                className="fab fa-instagram"
-              ></a>
-              <a
-                href="https://www.linkedin.com/"
-                className="fab fa-linkedin"
-              ></a>
-            </div>
-          </div>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gridRowGap: "90px",
+            gridColumnGap: "200px",
+          }}
+        >
+          {TeamList.map((member) => (
+            <MemberCard key={member.id} {...member} />
+          ))}
         </div>
-        {/* Additional team members can be added similarly */}
       </div>
     </section>
   );

55066e53633b6374ac2bd5c28fb6926daf74c447
SunsabParadon <Paradon@sunsab.com>
2024-05-21 20:51:23 -0600

    Fixed navigating to the section and center the contact's button

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,8 @@ import ComputerIcon from "@mui/icons-material/Computer";
 import PersonIcon from "@mui/icons-material/PersonOutlineOutlined";
 import SettingsIcon from "@mui/icons-material/SettingsOutlined";
 import transition from "../transition";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 
 function Services() {
   return (
@@ -19,28 +20,28 @@ function Services() {
           </div>
           <div className="services-grid">
             <div className="service-item">
-              <Link to="/portfolio">
+              <ScrollLink to="expertise" smooth={true} duration={500}>
+                <div className="item">
+                  <h6>OUR EXPERTISE</h6>
+                  <SettingsIcon style={{ color: "#FED136", fontSize: 80 }} />
+                </div>
+              </ScrollLink>
+            </div>
+            <div className="service-item">
+              <ScrollLink to="projects" smooth={true} duration={500}>
                 <div className="item">
                   <h6>PROJECTS</h6>
                   <ComputerIcon style={{ color: "#FED136", fontSize: 80 }} />
                 </div>
-              </Link>
+              </ScrollLink>
             </div>
             <div className="service-item">
-              <Link to="/team">
+              <ScrollLink to="team" smooth={true} duration={500}>
                 <div className="item">
                   <h6>THE TEAM</h6>
                   <PersonIcon style={{ color: "#FED136", fontSize: 80 }} />
                 </div>
-              </Link>
-            </div>
-            <div className="service-item">
-              <Link to="/expertise">
-                <div className="item">
-                  <h6>OUR EXPERTISE</h6>
-                  <SettingsIcon style={{ color: "#FED136", fontSize: 80 }} /> 
-                </div>
-              </Link>
+              </ScrollLink>
             </div>
           </div>
         </div>

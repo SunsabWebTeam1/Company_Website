61724542242996a97959eab9203eeb94fb1c9390
SunsabParadon <Paradon@sunsab.com>
2024-05-27 23:07:01 -0600

    Changed Drake Picture and key message at home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <section id="home" className="home">
       <h1>Software Design and Creation</h1>
-      <h2>Innovative Solutions</h2>
+      <h2>Digitalization Solutions</h2>
       <div className="wave wave1"></div>
       <div className="wave wave2"></div>
       <div className="wave wave3"></div>
